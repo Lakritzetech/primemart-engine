61e14d04537bf4d6a307829140bc8b2025766df5
Ogaitw <tiagoaugusto2402@gmail.com>
2023-07-07 18:53:13 -0300

    Ajustado CPF

--- src/main/java/com/system/market/entities/EmployeeRequestDTO.java
+++ src/main/java/com/system/market/entities/EmployeeRequestDTO.java
@@ -2,4 +2,7 @@ package com.system.market.entities;
 
 public record EmployeeRequestDTO(String name, String email, Long cpf, String shift, Long phone) {
 
+    public Long getCpf() {
+        return cpf;
+    }
 }
